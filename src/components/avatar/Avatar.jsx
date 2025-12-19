4bd318507b9438ce589d4c90afd614027e04cf07
QUANG-TRUNG <tqtrung3000@gmail.com>
2025-12-19 19:31:39 +0700

    test git

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,10 +1,12 @@
-import avatarDefault from './../../assets/avatar-default.svg'
-import './Avatar.css'
+import avatarDefault from "./../../assets/avatar-default.svg";
+import "./Avatar.css";
 
 function Avatar() {
   return (
-    <img src={avatarDefault} alt="avatar" className="avatar avatar-size-40" />
-  )
+    <>
+      <img src={avatarDefault} alt="avatar" className="avatar avatar-size-40" />
+    </>
+  );
 }
 
-export default Avatar
+export default Avatar;
